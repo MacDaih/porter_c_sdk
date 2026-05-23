7eb7ed1280f682d88bd14aaa4ab9789d79e60666
MacDaih <gbrldavidson@gmail.com>
2026-05-23 09:26:19 +0200

    patch publish packet->len field

--- packet.c
+++ packet.c
@@ -314,7 +314,7 @@ void make_publish(
     for(int i = 0; i < strlen(payload); i++)
         write_byte(payload[i], pkt);
 
-    pkt->len = strlen(pkt->payload);
+    pkt->len = rem_length + 1 + eval_bytes(rem_length);
     pkt->cursor = 0;
 }
 
